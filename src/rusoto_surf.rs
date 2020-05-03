579d796730f08dc819988441524ed36e0921f102
Jacob Rothstein <hi@jbr.me>
2020-05-03 14:27:13 -0700

    async-std reader for an rusoto bytestream

--- src/rusoto_surf.rs
+++ src/rusoto_surf.rs
@@ -1,19 +1,23 @@
-use rusoto_core::DispatchSignedRequest;
+use async_std::stream::StreamExt;
+use bytes::BufMut;
+use futures::FutureExt;
+use futures::Stream;
+use http::HeaderMap;
 use rusoto_core::request::HttpResponse;
 use rusoto_core::signature::SignedRequest;
-use std::time::Duration;
-use http::HeaderMap;
-use futures::FutureExt;
+use rusoto_core::ByteStream;
+use rusoto_core::DispatchSignedRequest;
 use std::convert::TryInto;
-use std::marker::Unpin;
 use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::Duration;
 
 #[derive(Default)]
 pub struct SurfRequestDispatcher;
 
 impl SurfRequestDispatcher {
-    pub fn new () -> Self {
-        SurfRequestDispatcher { }
+    pub fn new() -> Self {
+        SurfRequestDispatcher {}
     }
 }
 
@@ -33,12 +37,11 @@ async fn surf_req(mut rusoto_req: SignedRequest) -> surf::Result<surf::Response>
 
     if let Some(payload) = rusoto_req.payload.take() {
         pending_req = match payload {
-            rusoto_core::signature::SignedRequestPayload::Buffer(xs) => {
-                pending_req.body_bytes(xs)
-            },
-            rusoto_core::signature::SignedRequestPayload::Stream(_xs) => {
-                // TODO: how do we pipe a Stream into a surf request?
-                pending_req
+            rusoto_core::signature::SignedRequestPayload::Buffer(xs) => pending_req.body_bytes(xs),
+            rusoto_core::signature::SignedRequestPayload::Stream(xs) => {
+                let async_read = AsyncReader::new(xs);
+                let bufreader = async_std::io::BufReader::new(async_read);
+                pending_req.body(bufreader)
             }
         };
     }
@@ -46,46 +49,40 @@ async fn surf_req(mut rusoto_req: SignedRequest) -> surf::Result<surf::Response>
     pending_req.await
 }
 
-// TODO: This is a failed attempt at turning our AsyncRead Response object into a Stream suitable
-// for passing to the rusoto Response payload ByteStream.
-//
-// It very nearly worked: the bytestream would attempt to read via poll_next, and we'd forward the
-// read request in to the surf Response object. HOWEVER. Surf's Response object had already
-// consumed the response data, so we'd always get "0" bytes back.
-// 
-// Since the data is already consumed, I moved to the "body_bytes()" solution you see below, at
-// the next TODO.
-struct AsyncReadWrapper<R: futures::io::AsyncRead + Unpin> {
-    inner: R
+struct AsyncReader {
+    buffer: bytes::BytesMut,
+    stream: async_std::stream::Fuse<ByteStream>,
 }
 
-impl <R: futures::io::AsyncRead + Unpin + std::fmt::Debug> futures::stream::Stream for AsyncReadWrapper<R> {
-    type Item = Result<bytes::Bytes, std::io::Error>;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut futures::task::Context) -> futures::task::Poll<Option<Self::Item>> {
-
-        let myself = self.get_mut();
-        dbg!(&myself.inner);
-        let value = &mut myself.inner;
-        let pinned = Pin::new(value);
-        let mut buf = Vec::new();
-        match futures::io::AsyncRead::poll_read(pinned, cx, &mut buf) {
-            futures::task::Poll::Ready(result) => {
-                match result {
-                    Ok(bytes_read) => {
-                        let bytes_read = dbg!(bytes_read);
-                        if bytes_read == 0 {
-                            return futures::task::Poll::Ready(None);
-                        }
-                        futures::task::Poll::Ready(Some(Ok(bytes::Bytes::copy_from_slice(&buf[0..bytes_read]))))
-                    },
-                    Err(_e) => {
-                        futures::task::Poll::Ready(None)
-                    }
-                }
-            },
-            futures::task::Poll::Pending => futures::task::Poll::Pending,
+impl AsyncReader {
+    pub fn new(stream: ByteStream) -> Self {
+        AsyncReader {
+            buffer: bytes::BytesMut::new(),
+            stream: stream.fuse(),
         }
+    }
+}
+
+impl async_std::io::Read for AsyncReader {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<async_std::io::Result<usize>> {
+        if self.buffer.is_empty() {
+            match futures::ready!(Pin::new(&mut self.stream).poll_next(cx)) {
+                None => return Poll::Ready(Ok(0)),
+                Some(Err(e)) => return Poll::Ready(Err(e)),
+                Some(Ok(bytes)) => {
+                    self.buffer.put(bytes);
+                }
+            }
+        }
+        let available = std::cmp::min(buf.len(), self.buffer.len());
+        let bytes = self.buffer.split_to(available);
+        let (left, _) = buf.split_at_mut(available);
+        left.copy_from_slice(&bytes[..available]);
+        Poll::Ready(Ok(available))
     }
 }
 
@@ -97,20 +94,21 @@ impl DispatchSignedRequest for SurfRequestDispatcher {
     ) -> rusoto_core::request::DispatchSignedRequestFuture {
         let response_fut = surf_req(request);
 
-        let result = response_fut.then(async move |result| {
-            let mut response = result.unwrap();
+        let result = response_fut
+            .then(async move |result| {
+                let mut response = result.unwrap();
 
-            // TODO: Make this stream, vs. buffer the entire response in memory.
-            let body_bytes = response.body_bytes().await.unwrap();
+                // TODO: Make this stream, vs. buffer the entire response in memory.
+                let body_bytes = response.body_bytes().await.unwrap();
 
-            HttpResponse {
-                status: response.status().into(),
-                headers: HeaderMap::<String>::default(),
-                body: body_bytes.into()
-            }
-        }).map(Result::Ok);
+                HttpResponse {
+                    status: response.status().into(),
+                    headers: HeaderMap::<String>::default(),
+                    body: body_bytes.into(),
+                }
+            })
+            .map(Result::Ok);
 
         Box::pin(result)
     }
 }
-
