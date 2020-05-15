a1d940fcd64757000eefe3a769c387793de6a301
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-05-15 13:22:54 -0700

    remove unused box magic

--- src/auth/storage.rs
+++ src/auth/storage.rs
@@ -6,14 +6,6 @@ use std::any::Any;
 /// a struct or parameter sent by a corresponding `Scheme`.
 #[async_trait::async_trait]
 pub trait AuthnStorage<User: Send + Sync + 'static, Request: Any + Send + Sync + 'static> {
-    #[doc(hidden)]
-    async fn maybe_get_user(&self, request: Box<dyn Any + Send + Sync + 'static>) -> Result<Option<User>> {
-        match (request as Box<dyn Any + Send + 'static>).downcast::<Request>() {
-            Ok(boxed) => self.get_user(*boxed).await,
-            Err(_) => Ok(None)
-        }
-    }
-
     async fn get_user(&self, _request: Request) -> Result<Option<User>> {
         Ok(None)
     }
